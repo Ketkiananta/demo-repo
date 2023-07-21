afd823ebbc1373aeb6dec0d2dc58f0f807f764fb
ketki <ketkiananta@gmail.com>
2023-07-21 12:04:27 +0530

    second commit

---
+++ arraypractice.java
@@ -0,0 +1,23 @@
+package array;
+
+public class arraypractice {
+    public static void main(String[] args) {
+      /*  String [] s1 ={"one","two","three","four"};
+
+        String [] s2 ={"four","six","three","seven"};
+
+        for (int i=0; i<s1.length; i++){
+            for (int j=0; j<s2.length; j++){
+                if(s1[i]==s2[j]){
+                    System.out.println(s1[i]);
+                }
+            }
+        }*/
+
+        String s= "ketki";
+        for(int i=s.length()-1; i>=0; i--){
+            System.out.print(s.charAt(i));
+        }
+
+    }
+        }
